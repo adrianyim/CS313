75f1f74a02abe52dc4008087d0b6884c81ff3c49
adrianyim <adriany93@gmail.com>
2019-05-03 12:48:26 -0600

    updated week02 assignment

--- web/Week02/assignment/home.php
+++ web/Week02/assignment/home.php
@@ -12,9 +12,9 @@
     include 'header.php';
     ?>
     <div class="div-info"><img src="me.jpg" class="me">
-        <p>My name is Adrian Yim. I am from Hong Kong and I am majoring in software engineering. I have been married for more than 4 months. I know three languages: Cantonese, Engllish, and Mandrian.</p>
-        <p>When I was in high school, I used to make installation arts. I also like playing sports, especially soccer, pang ping, and tennis. </p>
+        <p class="content-left">My name is Adrian Yim. I am from Hong Kong and I am majoring in software engineering. I have been married for more than 4 months. I know three languages: Cantonese, Engllish, and Mandrian.</p>
+        <p class="content-right">When I was in high school, I used to make installation arts. I also like playing sports, especially soccer, pang ping, and tennis. </p>
         <img src="my-art.jpg" class="my-art">
-        <p>I worked as Quality Assureance Engineer Internship in the Corporation of the Presiding Bishop of the Church of Jesus Christ of Latter-day Saints in 2018. This experience has been helping me to be awared of the important of software quality.</p></div>
+        <p class="content-left">I worked as Quality Assureance Engineer Internship in the Corporation of the Presiding Bishop of the Church of Jesus Christ of Latter-day Saints in 2018. This experience has been helping me to be awared of the important of software quality.</p></div>
 </body>
 </html>
