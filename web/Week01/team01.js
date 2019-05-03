55f6a387665c1363523d8fa2f2c51fcaabd83279
adrianyim <adriany93@gmail.com>
2019-05-02 18:29:26 -0600

    updated 2 files

---
+++ web/Week01/team01.js
@@ -0,0 +1,8 @@
+function alartMe() {
+    window.alert("Clicked!!");
+}
+
+function changeColor() {
+    var userColor = document.getElementById("userColor").value;
+    document.getElementById("div1").style.color = userColor;
+}
