64d76aca8b8cb90a690fdd79dc93e8c406f882dc
adrianyim <adriany93@gmail.com>
2019-05-03 10:54:05 -0600

    updated week02 assignment

--- web/Week02/assignment/header.php
+++ web/Week02/assignment/header.php
@@ -8,9 +8,8 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
         <a href="/Week02/assignment/home.php" <?php if ($file === "home") echo 'active' ?> class="a-header">Home</a>
         <a href="/Week02/assignment/assignment.php" <?php if ($file === "assignment") echo 'active' ?> class="a-header">Assignment</a>
         <?php 
-        echo "Today's date is :"; 
-        $today = date("d/m/Y"); 
-        echo $today; 
+        $today = date("m/d/Y");
+        echo "Today's date is : $today"; 
         ?> 
     </div>
     <hr>
