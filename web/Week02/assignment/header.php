085e917087127bd21059936ec6728a7f4afcc122
adrianyim <adriany93@gmail.com>
2019-05-03 13:03:15 -0600

    updated week02 assignment

--- web/Week02/assignment/header.php
+++ web/Week02/assignment/header.php
@@ -8,10 +8,10 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
         <a href="/Week02/assignment/home.php" <?php if ($file === "home") echo 'active' ?> class="a-header">Home</a>
         <a href="/Week02/assignment/assignment.php" <?php if ($file === "assignment") echo 'active' ?> class="a-header">Assignment</a>
         <span id='time'>
-        <span <?php
+        <span> <?php
         date_default_timezone_set("America/Denver");
         echo "Mountain Time " . date("l, M d, Y"); 
-        ?>>
+        ?></span>
     </div>
     <hr>
     <script src="home.js"></script>
