39bc7718004e7e4a5e01ac5a2c52c7b1138ad3b9
adrianyim <adriany93@gmail.com>
2019-05-03 14:01:28 -0600

    updated week02 assignment

--- web/Week02/assignment/footer.php
+++ web/Week02/assignment/footer.php
@@ -4,9 +4,13 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 
 <footer>
     <hr>
-    <p>Contact:</p>
-    <p>
-        <span><a href="https://www.facebook.com/adrian.yim.00"><img src="facebook-logo.png"></a></span>
-        <span><a href="https://www.linkedin.com/in/adrian-yim/"><img src="linkedin-logo.png"></span></p>
-    <p>© 2019-<?php echo date("Y");?></p>
+    <div class="div-footer">
+        <p>Contact:</p>
+        <p>
+            <span><a href="https://www.facebook.com/adrian.yim.00"><img src="facebook-logo.png"></a></span>
+            
+            <span><a href="https://www.linkedin.com/in/adrian-yim/"><img src="linkedin-logo.png"></span>
+        </p>
+        <p>© 2019-<?php echo date("Y");?></p>
+    </div>
 </footer>
