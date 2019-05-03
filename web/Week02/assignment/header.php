46d6f32d02c3eacc7b01f8189f6e4a2fd12a215b
adrianyim <adriany93@gmail.com>
2019-05-03 11:18:32 -0600

    updated week02 assignment

--- web/Week02/assignment/header.php
+++ web/Week02/assignment/header.php
@@ -7,7 +7,8 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
     <div class="div-header">
         <a href="/Week02/assignment/home.php" <?php if ($file === "home") echo 'active' ?> class="a-header">Home</a>
         <a href="/Week02/assignment/assignment.php" <?php if ($file === "assignment") echo 'active' ?> class="a-header">Assignment</a>
-        <?php 
+        <?php
+        date_default_timezone_set("MT"); 
         $timestamp = time();
         echo(date("H:i:s a, m/d/Y", $timestamp)); 
         ?> 
