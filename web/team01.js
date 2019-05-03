afc11ab15313d9c289a032c325144684a99b0f48
adrianyim <adriany93@gmail.com>
2019-05-02 18:41:42 -0600

    testing new heroku

--- web/team01.js
+++
@@ -1,8 +0,0 @@
-function alartMe() {
-    window.alert("Clicked!!");
-}
-
-function changeColor() {
-    var userColor = document.getElementById("userColor").value;
-    document.getElementById("div1").style.color = userColor;
-}
