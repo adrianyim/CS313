7d7c926db044a4735f78db49a4665f2e5aa53985
adrianyim <adriany93@gmail.com>
2019-05-02 22:47:40 -0600

    updated week02 assignment

--- web/Week02/assignment/header.php
+++ web/Week02/assignment/header.php
@@ -4,9 +4,9 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 
 <header>
     <a href="http://www.byui.edu"><img src="byui-logo.jpg" class="byui-logo"></a>
-    <div class="div-header">
-        <a href="/Week02/assignment/home.php" <?php if ($file === "home") echo 'active' ?>>Home</a>
-        <a href="/Week02/assignment/assignment.php" <?php if ($file === "assignment") echo 'active' ?>>Assignment</a>
+    <div>
+        <a href="/Week02/assignment/home.php" <?php if ($file === "home") echo 'active' ?> class="a-header">Home</a>
+        <a href="/Week02/assignment/assignment.php" <?php if ($file === "assignment") echo 'active' ?> class="a-header">Assignment</a>
     </div>
     <hr>
 </header>
