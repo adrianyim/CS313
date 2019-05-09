61dd7b9efdf34c10f87f584abb6cad42af0533e4
adrianyim <adriany93@gmail.com>
2019-05-08 18:36:25 -0600

    Updated Week03 team activity

--- web/Week03/activityweek3.php
+++ web/Week03/activityweek3.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <title>Team Activity Week03</title>
     <link rel="stylesheet" type="text/css" href="../../style/main.css">
-    <title>Document</title>
 </head>
 <body>
 
@@ -17,7 +16,6 @@ $NAME = $_POST["Name"];
 $EMAIL = $_POST["Email"];
 $MAJOR = $_POST["Major"];
 $COMMENT = $_POST["Comments"];
-
 $VISITED  = $_POST["Visited"];
 
 }
@@ -27,21 +25,23 @@ include '../../header.php';
 
 
 <form method="post" action="<?PHP htmlspecialchars($_SERVER["PHP_SELF"]); ?>" > 
-
+Name:
 <input type="text" placeholder="Name" name="Name">
 <br>
+Email:
 <input tyoe="text" placeholder="Email" name="Email">
 <br>
-
+Major:
 <input type="radio" value="CS"  name="Major"> Computer Science
 <input type="radio" value="WDD" name="Major"> Web Design and Development
 <input type="radio" value="CIT" name="Major"> Computer Information Technology
 <input type="radio" value="CE"  name="Major"> Computer Engineering
 
 <br>
+Comments:
 <textarea name="Comments" rows="5" cols="40" placeholder="Enter Comments Here"></textarea>
 <br>
-
+Visited Place:
 <input type="checkbox" name="Visited[]" value="North America">North America<br>
 <input type="checkbox" name="Visited[]" value="South America">South America<br>
 <input type="checkbox" name="Visited[]" value="Europe">Europe<br>
@@ -55,7 +55,6 @@ include '../../header.php';
 
 </form>
 <br>
-
 
 <hr>
 <p>
