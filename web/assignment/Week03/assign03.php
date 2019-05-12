6677520d2402e7f928dd6d0d398356286588aa8d
adrianyim <adriany93@gmail.com>
2019-05-11 20:57:38 -0600

    Updated Week03 assignment

--- web/assignment/Week03/assign03.php
+++ web/assignment/Week03/assign03.php
@@ -1,5 +1,47 @@
 <?php
     session_start();
+
+    //Define the items and cost
+    $Items = array("Plan of Salvation", "Consider the Lilies", "Five Loaves and Two Fishes", "Tiger", "House");
+    $cost = array("$500", "$300", "250", "$150", "$150");
+
+    //Load up session
+    if (!isset($_SESSION["TOTAL"])) {
+        $_SESSION["TOTAL"] = 0;
+        for ($i = 0; $i < count($Items); $i++) {
+            $_SESSION["QTY"] [$i] = 0;
+            $_SESSION["COST"] [$i] = 0;
+        }
+    }
+
+    //Reset
+    if (isset($_POST["reset"])) {
+        if ($_POST["reset"] == "true") {
+            unset($_SESSION["QTY"]);
+            unset($_SESSION["COST"]);
+            unset($_SESSION["TOTAL"]);
+            unset($_SESSION["CART"]);
+        }
+    }
+
+    //Add
+    if (isset($_POST["add"])) {
+        $i = $_POST["add"];
+        $qty = $_SESSION["QTY"][$i] + 1;
+        $_SESSION["COST"][$i] = $cost[$i] * $qty;
+        $_SESSION["CART"][$i] = $i;
+        $_SESSION["QTY"][$i] = $qty;
+    }
+
+    //Delete
+    if (isset($_POST["delete"])) {
+        $i = $_POST["delete"];
+        $qty = $_SESSION["QTY"][$i];
+        $qty--;
+        $_SESSION["QTY"][$i] = $qty;
+    } else {
+        $_SESSION["COST"][$i] = $cost[$i] * $qty;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +56,77 @@
 <?php
 include 'header.php';
 ?>
+
+<h2>List of All Products</h2>
+ <table>
+   <tr>
+   <th>Product</th>
+   <th width="10px">&nbsp;</th>
+   <th>Cost</th>
+   <th width="10px">&nbsp;</th>
+   <th>Action</th>
+   </tr>
+ <?php
+ for ($i=0; $i< count($Items); $i++) {
+   ?>
+   <tr>
+   <td><?php echo($Items[$i]); ?></td>
+   <td width="10px">&nbsp;</td>
+   <td><?php echo($cost[$i]); ?></td>
+   <td width="10px">&nbsp;</td>
+   <td><a href="?add=<?php echo($i); ?>">Add to cart</a></td>
+   </tr>
+   <?php
+ }
+ ?>
+ <tr>
+ <td colspan="5"></td>
+ </tr>
+ <tr>
+ <td colspan="5"><a href="?reset=true">Reset Cart</a></td>
+ </tr>
+ </table>
+ <?php
+ if ( isset($_SESSION["CART"]) ) {
+ ?>
+ <br/><br/><br/>
+ <h2>Cart</h2>
+ <table>
+ <tr>
+ <th>Item</th>
+ <th width="10px">&nbsp;</th>
+ <th>Qty</th>
+ <th width="10px">&nbsp;</th>
+ <th>Cost</th>
+ <th width="10px">&nbsp;</th>
+ <th>Action</th>
+ </tr>
+ <?php
+ $total = 0;
+ foreach ( $_SESSION["cart"] as $i ) {
+ ?>
+ <tr>
+ <td><?php echo( $Items[$_SESSION["CART"][$i]] ); ?></td>
+ <td width="10px">&nbsp;</td>
+ <td><?php echo( $_SESSION["QTY"][$i] ); ?></td>
+ <td width="10px">&nbsp;</td>
+ <td><?php echo( $_SESSION["COST"][$i] ); ?></td>
+ <td width="10px">&nbsp;</td>
+ <td><a href="?delete=<?php echo($i); ?>">Delete from cart</a></td>
+ </tr>
+ <?php
+ $total = $total + $_SESSION["COST"][$i];
+ }
+
+ $_SESSION["TOTAL"] = $total;
+ ?>
+ <tr>
+ <td colspan="7">Total : <?php echo($total); ?></td>
+ </tr>
+ </table>
+ <?php
+ }
+ ?>
 
     <div class="div-info">
         <h2>Chinese Paper Cutting online shipping</h2>
