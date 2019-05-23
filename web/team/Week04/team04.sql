8029dc623debaae2494748664c703e0d8947995b
adrianyim <adriany93@gmail.com>
2019-05-23 15:01:31 -0600

    Updated Week04 assignment

--- web/team/Week04/team04.sql
+++ web/team/Week04/team04.sql
@@ -1,24 +1,24 @@
 CREATE TABLE users (
-	user_id INT	NOT NULL CONSTRAINT,
+	user_id INT	NOT NULL,
 	username VARCHAR (45) NOT NULL,
 	PRIMARY KEY (user_id)
 );
 
 CREATE TABLE speaker (
-	speaker_id INT NOT NULL CONSTRAINT,
-	speaker_name VARCHAR (45) CONSTRAINT NOT NULL,
+	speaker_id INT NOT NULL,
+	speaker_name VARCHAR (45) NOT NULL,
 	PRIMARY KEY (speaker_id)
 );
 
 CREATE TABLE conference_session (
-	conference_session_id INT NOT NULL CONSTRAINT,
+	conference_session_id INT NOT NULL,
 	session_date date CONSTRAINT session_date NOT NULL,
 	session_time INT CONSTRAINT session_time NOT NULL,
 	PRIMARY KEY (conference_session_id)
 );
 
 CREATE TABLE speaker_session (
-	speaker_session_id INT NOT NULL CONSTRAINT,
+	speaker_session_id INT NOT NULL,
 	speaker_id INT CONSTRAINT speaker_id NOT NULL, 
 	session_id INT CONSTRAINT session_id NOT NULL,
 	PRIMARY KEY (speaker_session_id),
@@ -27,7 +27,7 @@ CREATE TABLE speaker_session (
 );
 
 CREATE TABLE notes (
-	note_id INT NOT NULL CONSTRAINT,
+	note_id INT NOT NULL,
 	speaker_session INT CONSTRAINT speaker_session NOT NULL,
 	conference_session INT CONSTRAINT conference_session NOT NULL,
 	note_content VARCHAR (500) CONSTRAINT note_content NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE notes (
 
 SELECT * FROM users;
 
-INSERT INTO users VALUES ('001', 'Adrian Yim');
+INSERT INTO users VALUES ('003', 'Adrian Yim', 'M');
 
 INSERT INTO speaker (speaker_id, speaker_name) 
 	VALUES 	(1, 'Ulisses Soares'),
