2ba0ac9057c9c3c4652868887115b92594d33831
adrianyim <adriany93@gmail.com>
2019-05-29 21:45:19 -0600

    Updated my DB

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,3 +1,5 @@
+-- Create section
+
 CREATE TABLE users (
     user_id INT NOT NULL,
     user_name VARCHAR(45) NOT NULL,
@@ -9,16 +11,9 @@ CREATE TABLE item (
     item_id INT NOT NULL,
     item VARCHAR(45) NOT NULL,
     item_type VARCHAR(45) NOT NULL,
-    remark VARCHAR(45),
-    PRIMARY KEY (item_id),
-    FOREIGN KEY (item_id, item) REFERENCES cost(cost_id)
-);
-
-CREATE TABLE cost (
-    item_id INT NOT NULL,
-    cost_id INT NOT NULL,
     cost DECIMAL NOT NULL,
     cost_type VARCHAR(45) NOT NULL,
+    remark VARCHAR(45),
     PRIMARY KEY (item_id)
 );
 
@@ -33,22 +28,42 @@ CREATE TABLE summary (
     FOREIGN KEY (item_id) REFERENCES item(item_id)
 );
 
+-- Insert section
+
 INSERT INTO users (user_id, user_name, gender)
-    VALUES  (001, 'Tester 1', 'M'),
-            (002, 'Tester 2', 'F');
+    VALUES  (101, 'Tester 1', 'M'),
+            (102, 'Tester 2', 'F'),
+            (103, 'Adrian', 'M'),
+            (104, 'Olivia', 'F'),
+            (105, 'Quinton', 'M'),
+            (106, 'Angus', 'M'),
+            (107, 'Travis', 'M'),
+            (108, 'Eva', 'F'),
+            (109, 'Abigail', 'F'),
+            (110, 'Hazel', 'F'),
+            (111, 'Adrianna', 'F'),
+            (112, 'Ariana', 'F');
 
-INSERT INTO item (item_id, item, item_type, remark)
-    VALUES  (0001, 'Walmark', 'expense', 'shoes'),
-            (0002, 'BBQ', 'expense', ''),
-            (0003, 'himhim salary', 'income', 'May');
+INSERT INTO item (item_id, item, item_type, cost, cost_type, remark)
+    VALUES  (1001, 'Walmark', 'Food', 20.99, 'expense', 'shoes'),
+            (1002, 'BBQ', 'Food', 11.02, 'expense', ''),
+            (1003, 'Tester 1 salary', 'Salaries and wages', 504.32, 'income', 'May'),
+            (1004, 'Walmart', 'Food', 30.51, 'expense', ''),
+            (1005, 'phone bill', 'Utility expenses', 22.39, 'expense', ''),
+            (1006, 'Deseret Book', 'Others', 12.71, 'expense', 'BOM'),
+            (1007, 'pizza', 'Food', 10.59, 'expense', 'BYUI crossroad'),
+            (1008, 'burger king', 'Food', 13.76, 'expense', ''),
+            (1009, 'gas', 'Utility expenses', 20.11, 'expense', ''),
+            (1010, 'tithing', 'Others', 23.2, 'expense', 'May'),
+            (1011, 'Rent', 'Utility expenses', 573, 'income', '??');
 
-INSERT INTO cost (item_id, cost_id, cost, cost_type)
-    VALUES  (0001, 1, 20.99, 'expense'),
-            (0002, 2, 11.02, 'expense'),
-            (0003, 3, 500, 'income');
 
-SELECT * FROM  cost;
+-- Command my DB
 
-DROP TABLE cost;
+SELECT * FROM  users;
 
-UPDATE cost SET cost = 498.9 WHERE item_id = 0003;
+DROP TABLE item;
+
+UPDATE users 
+SET user_id = 101
+WHERE user_name = "Tester 1";
