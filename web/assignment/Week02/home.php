040a2b4d8bac897cb514928edaa6b621341cca5e
adrianyim <adriany93@gmail.com>
2019-06-06 15:51:41 -0600

    Updated home.css

--- web/assignment/Week02/home.php
+++ web/assignment/Week02/home.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="/assignment/home.css">
 </head>
 <body>
-    <?php
-    include 'header.php'
-    ?>
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/assignment/header.php';
+?>
     <div class="div-info"><img src="me.jpg" class="me">
         <p class="content-left">My name is Adrian Yim. I am from Hong Kong and I am majoring in software engineering. I have been married for more than 4 months. I know three languages: Cantonese, Engllish, and Mandrian.</p>
         <p class="content-right">When I was in high school, I used to make installation arts. I also like playing sports, especially soccer, pang ping, and tennis. </p>
         <img src="my-art.jpg" class="my-art">
         <p class="content-left">I worked as Quality Assureance Engineer Internship in the Corporation of the Presiding Bishop of the Church of Jesus Christ of Latter-day Saints in 2018. This experience has been helping me to be awared of the important of software quality.</p></div>
-    <?php
-    include 'footer.php'
-    ?>
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/assignment/footer.php';
+?>
 </body>
 </html>
