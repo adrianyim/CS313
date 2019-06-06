d51c3a7c6aa2a59804054c60f009a40a5e1ae503
adrianyim <adriany93@gmail.com>
2019-06-05 20:09:21 -0600

    Updated budget-planner

--- web/assignment/Week05-07/insertDB.php
+++ web/assignment/Week05-07/insertDB.php
@@ -12,11 +12,11 @@ $db = getDB();
 
 var_dump('$user, $gender');
 
-$statement = $db->prepare('INSERT INTO users(user_id, user_name, gender) VALUES (DEFAULT, :user, :gender);');
-$statement->bindValue(':user', $user, PDO::PARAM_STR);
-$statement->bindValue(':gender', $gender, PDO::PARAM_STR_CHAR);
-var_dump('get state 3');
-$statement->execute();
+$statement = $db->query('INSERT INTO users(user_id, user_name, gender) VALUES (DEFAULT, $user, $gender);');
+// $statement->bindValue(':user', $user, PDO::PARAM_STR);
+// $statement->bindValue(':gender', $gender, PDO::PARAM_STR_CHAR);
+// var_dump('get state 3');
+// $statement->execute();
 
 var_dump('executed');
 
