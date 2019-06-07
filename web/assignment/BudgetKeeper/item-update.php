226baa97c78636039c94293dea0d7aa3392586b3
adrianyim <adriany93@gmail.com>
2019-06-06 21:45:47 -0600

    Updated budget-keeper.php

--- web/assignment/BudgetKeeper/item-update.php
+++ web/assignment/BudgetKeeper/item-update.php
@@ -19,9 +19,9 @@ include $_SERVER['DOCUMENT_ROOT'].'/assignment/header.php';
 <div>
     <table>
         <form method="post" action="updateDB.php">
-            <tr><td>Item: <input type="text" name="Item" placeholder="Item" value="<?php echo $_SESSION['item'] ?>"></td></tr>
-            <tr><td>Item Type: <?php echo $_SESSION['item_type'] ?><select name="Item-type">
-            <option value="empty">--Item Type--</option>
+            <tr><td>Item: <input type="text" name="Item" value="<?php echo $_SESSION['item'] ?>"></td></tr>
+            <tr><td>Item Type: <select name="Item-type">
+            <option value="<?php echo $_SESSION['item_type'] ?>"><?php echo $_SESSION['item_type'] ?></option>
             <option value="Salaries and wages">Salaries and wages</option>
             <option value="Utility expenses">Utility expenses</option>
             <option value="Administration expenses">Administration expenses</option>
@@ -31,10 +31,10 @@ include $_SERVER['DOCUMENT_ROOT'].'/assignment/header.php';
             <option value="Food">Food</option>
             <option value="Others">Others</option>
             </select></td></tr>
-            <tr><td>Cost: <?php echo $_SESSION['cost'] ?><input type="text" name="Cost" placeholder="Cost"></td></tr>
+            <tr><td>Cost: <input type="text" name="Cost" value="<?php echo $_SESSION['cost'] ?>"></td></tr>
             <tr><td>Cost Type: <?php echo $_SESSION['cost_type'] ?><input type="radio" name="Cost-type" value="Income">Income
           <input type="radio" name="Cost-type" value="Expense">Expense</td></tr>
-            <tr><td>Remark: <?php echo $_SESSION['remark'] ?><textarea name="Remark" placeholder="Remark"></textarea></td></tr>
+            <tr><td>Remark: <textarea name="Remark" value="<?php echo $_SESSION['remark'] ?>"></textarea></td></tr>
             <tr><td><input type="submit" value="Submit"></td></tr>
         </form>
     </table>
