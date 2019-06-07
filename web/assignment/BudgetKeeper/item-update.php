76f3d0310d70fcb816d9dd11757c9600e28d43e1
adrianyim <adriany93@gmail.com>
2019-06-06 21:33:32 -0600

    Updated updateDB.php

--- web/assignment/BudgetKeeper/item-update.php
+++ web/assignment/BudgetKeeper/item-update.php
@@ -25,8 +25,8 @@ include $_SERVER['DOCUMENT_ROOT'].'/assignment/header.php';
 <div>
     <table>
         <form method="post" action="updateDB.php">
-            <tr><td>Item: <input type="text" name="Item" placeholder="Item"></td></tr>
-            <tr><td>Item Type: <select name="Item-type">
+            <tr><td>Item: <input type="text" name="Item" placeholder="Item" value="<?php echo $_SESSION['item'] ?>"></td></tr>
+            <tr><td>Item Type: <?php echo $_SESSION['item_type'] ?><select name="Item-type">
             <option value="empty">--Item Type--</option>
             <option value="Salaries and wages">Salaries and wages</option>
             <option value="Utility expenses">Utility expenses</option>
@@ -37,9 +37,9 @@ include $_SERVER['DOCUMENT_ROOT'].'/assignment/header.php';
             <option value="Food">Food</option>
             <option value="Others">Others</option>
             </select></td></tr>
-            <tr><td>Cost: <input type="text" name="Cost" placeholder="Cost"></td></tr>
-            <tr><td>Cost Type: <input type="radio" name="Cost-type" value="Expense"></td></tr>
-            <tr><td>Remark: <textarea name="Remark" placeholder="Remark"></textarea></td></tr>
+            <tr><td>Cost: <?php echo $_SESSION['cost'] ?><input type="text" name="Cost" placeholder="Cost"></td></tr>
+            <tr><td>Cost Type: <?php echo $_SESSION['cost_type'] ?><input type="radio" name="Cost-type" value="Expense"></td></tr>
+            <tr><td>Remark: <?php echo $_SESSION['remark'] ?><textarea name="Remark" placeholder="Remark"></textarea></td></tr>
             <tr><td><input type="submit" value="Submit"></td></tr>
         </form>
     </table>
