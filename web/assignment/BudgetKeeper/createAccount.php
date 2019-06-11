d17939e1f3480362ae85bf8828692e4d08bb8884
adrianyim <adriany93@gmail.com>
2019-06-11 14:44:25 -0600

    Updated signUp.php

--- web/assignment/BudgetKeeper/createAccount.php
+++ web/assignment/BudgetKeeper/createAccount.php
@@ -5,18 +5,18 @@ $gender = htmlspecialchars($_POST['Gender']);
 
 if (!isset($username) || $username == "" || !isset($password) || $password == "" || !isset($gender) || $gender == "")
 {
-    var_dump('Username or password is incorrected!!');
+    var_dump('Username or password or gender is incorrected!!');
     header("Location: signUp.php");
     die();
 }
 
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-require("connectDB.php");
+require('connectDB.php');
 $db = get_db();
 
 
-$statement = $db->prepare('INSERT INTO users (user_id, user_name, password, gender) VALUES(DEFAULT, :username, :password, :gender)');
+$statement = $db->prepare('INSERT INTO users(user_id, user_name, password, gender) VALUES(DEFAULT, :username, :password, :gender);');
 $statement->bindValue(':username', $username);
 $statement->bindValue(':password', $hashedPassword);
 $statement->bindValue(':gender', $gender);
