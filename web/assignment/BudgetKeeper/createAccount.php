7b4f22558172fae7edc197fdee8a9e4544936a80
adrianyim <adriany93@gmail.com>
2019-06-10 23:23:34 -0600

    Updated signUp.php

--- web/assignment/BudgetKeeper/createAccount.php
+++ web/assignment/BudgetKeeper/createAccount.php
@@ -6,7 +6,7 @@ $gender = $_POST['Gender'];
 if (!isset($username) || $username == "" || !isset($password) || $password == "" || !isset($gender) || $gender == "")
 {
     header("Location: signUp.php");
-    var_dump("<?php echo 'False'?>");
+    var_dump('False');
     die();
 }
 
@@ -17,13 +17,15 @@ $gender = htmlspecialchars($gender);
 require("connectDB.php");
 $db = get_db();
 
+var_dump('connected db');
+
 $statement = $db->prepare('INSERT INTO users (user_id, username, password, gender) VALUES(DEFAULT, :username, :password, :gender)');
 $statement->bindValue(':username', $username);
 $statement->bindValue(':password', $hashedPassword);
 $statement->bindValue(':gender', $gender);
+var_dump('true');
 $statement->execute();
 
-var_dump("<?php echo 'true'?>");
 
 header("Location: login.php");
 die();
