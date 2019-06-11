1a3cb79aa1aa7746f723bd8544d4c3abc8c16a66
adrianyim <adriany93@gmail.com>
2019-06-10 20:57:57 -0600

    Updated signUp.php

--- web/assignment/Week02/assignment.php
+++ web/assignment/Week02/assignment.php
@@ -13,20 +13,20 @@ include $_SERVER['DOCUMENT_ROOT'].'/assignment/header.php';
 ?>
     <div class="div-assign-content">
         <ul>
-            <li class="assign_List"><a href="/assignment/Week01/hello.html">Assignment 01</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/home.php">Assignment 02</a></li>
-            <li class="assign_List"><a href="/assignment/Week03/assign03.php">Assignment 03</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign04.php">Assignment 04</a></li>
-            <li class="assign_List"><a href="/assignment/Week05/assign05.php">Assignment 05</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign06.php">Assignment 06</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign07.php">Assignment 07</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign08.php">Assignment 08</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign09.php">Assignment 09</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign10.php">Assignment 10</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign11.php">Assignment 11</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign12.php">Assignment 12</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign13.php">Assignment 13</a></li>
-            <li class="assign_List"><a href="/assignment/Week02/assign14.php">Assignment 14</a></li>
+            <li class="links"><a href="/assignment/Week01/hello.html">Assignment 01</a></li>
+            <li class="links"><a href="/assignment/Week02/home.php">Assignment 02</a></li>
+            <li class="links"><a href="/assignment/Week03/assign03.php">Assignment 03</a></li>
+            <li class="links"><a href="/assignment/Week02/assign04.php">Assignment 04</a></li>
+            <li class="links"><a href="/assignment/Week05/assign05.php">Assignment 05</a></li>
+            <li class="links"><a href="/assignment/Week02/assign06.php">Assignment 06</a></li>
+            <li class="links"><a href="/assignment/Week02/assign07.php">Assignment 07</a></li>
+            <li class="links"><a href="/assignment/Week02/assign08.php">Assignment 08</a></li>
+            <li class="links"><a href="/assignment/Week02/assign09.php">Assignment 09</a></li>
+            <li class="links"><a href="/assignment/Week02/assign10.php">Assignment 10</a></li>
+            <li class="links"><a href="/assignment/Week02/assign11.php">Assignment 11</a></li>
+            <li class="links"><a href="/assignment/Week02/assign12.php">Assignment 12</a></li>
+            <li class="links"><a href="/assignment/Week02/assign13.php">Assignment 13</a></li>
+            <li class="links"><a href="/assignment/Week02/assign14.php">Assignment 14</a></li>
         </ul>
     </div>
 <?php
