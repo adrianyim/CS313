5b1bd870f83a4e0f1d0267ed892770d74380fb68
adrianyim <adriany93@gmail.com>
2019-06-11 21:32:53 -0600

    updated header.php

--- web/assignment/header.php
+++ web/assignment/header.php
@@ -13,6 +13,7 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
             echo "Mountain Time " . date("l, m-d-Y"); 
             ?>
         </span>
+        <span><label>User: <?php echo $_SESSION['username']; ?></label></span>
     </div>
     <hr>
     <script src="home.js"></script>
