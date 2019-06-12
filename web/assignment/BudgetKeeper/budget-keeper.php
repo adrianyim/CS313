8af25af5ef1d55b84541e43f3a53e8d76d4674e3
adrianyim <adriany93@gmail.com>
2019-06-11 22:01:18 -0600

    updated budget-keeper.php

--- web/assignment/BudgetKeeper/budget-keeper.php
+++ web/assignment/BudgetKeeper/budget-keeper.php
@@ -22,7 +22,7 @@ $items = $db->query('SELECT item_id, item, item_type, cost, cost_type, remark FR
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/assignment/header.php';
 ?>
-<label>User: <?php echo $_SESSION['username']; ?></label><span><label class="links"><a href="login.php">Log Out</a></label></span>
+<label>User: <?php echo $_SESSION['username']; ?></label><label class="links"><a href="login.php"> Log Out</a></label>
   <div class="div-info">
     <div>
       <table class="table-insert">
